3483ec18f46fd5608ce988f3e3a615e361973f05
Rikiricks <riky92.ce@gmail.com>
2021-11-30 23:10:10 +0530

    Add the express

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const http = require("http");
-const routes = require("./routes");
+//const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer(routes);
+const app = express();
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-server.listen(3000);
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("add-product");
+    res.send("<form action='/product' method='POST'><input type='text' name='message'/><input type='number' name='Age'/><button type='submit'>Send</button></form>");
+    //next();
+});
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+});
+
+
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Hello</h1>");   
+    
+});
+
+// const server = http.createServer(app);
+// server.listen(3000);
+
+app.listen(3000)
 
